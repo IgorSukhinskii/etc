6da8e0f5fad9cd9b624d4eb6eba3ea4041edd51c
Igor Sukhinskii <igor.sukhinskii@mirasys.com>
2025-11-22 01:59:45 +0200

    add nvf (neovim flake configuration)

--- flake.nix
+++ flake.nix
@@ -3,15 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:nix-community/stylix";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, stylix }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, stylix, nvf }:
 
   {
     # Build darwin flake using:
@@ -22,6 +32,9 @@
         home-manager.darwinModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.sharedModules = [
+            nvf.homeManagerModules.default
+          ];
           home-manager.users."igor.sukhinskii" = import ./home.nix;
         }
         stylix.darwinModules.stylix
--- home.nix
+++ home.nix
@@ -75,13 +75,24 @@
     enable = true;
   };
 
-  programs.neovim = {
+  programs.nvf = {
     enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
+
+    settings = {
+      vim.viAlias = true;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
   };
+ #programs.neovim = {
+ #  enable = true;
+ #  defaultEditor = true;
+ #  viAlias = true;
+ #  vimAlias = true;
+ #  vimdiffAlias = true;
+ #};
 
   programs.zellij = {
     enable = true;
