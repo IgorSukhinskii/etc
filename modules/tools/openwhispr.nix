c21fe8ad94afe4789f50f8c804c6b09b880367bf
Igor Sukhinskii <igor.sukhinskii@gmail.com>
2026-03-22 19:26:45 +0200

    feat: add openwhispr

---
+++ modules/tools/openwhispr.nix
@@ -0,0 +1,28 @@
+{ ... }:
+{
+  flake.homeManagerModules.openwhispr =
+    { pkgs, lib, ... }:
+    {
+      home.activation.openwhispr = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+        /usr/bin/xattr -cr "$HOME/Applications/Home Manager Apps/OpenWhispr.app" 2>/dev/null || true
+      '';
+
+      home.packages = [
+        (pkgs.stdenvNoCC.mkDerivation {
+          pname = "openwhispr";
+          version = "1.6.5";
+          src = pkgs.fetchurl {
+            url = "https://github.com/OpenWhispr/openwhispr/releases/download/v1.6.5/OpenWhispr-1.6.5-arm64.dmg";
+            hash = "sha256-qPT/pftSixffB2ULIureiiu9YodvxKTSiWg7IlfMmNM=";
+          };
+          nativeBuildInputs = [ pkgs.undmg ];
+          sourceRoot = ".";
+          installPhase = ''
+            mkdir -p $out/Applications
+            cp -r *.app $out/Applications/
+            /usr/bin/codesign --force --deep --sign - $out/Applications/*.app
+          '';
+        })
+      ];
+    };
+}
